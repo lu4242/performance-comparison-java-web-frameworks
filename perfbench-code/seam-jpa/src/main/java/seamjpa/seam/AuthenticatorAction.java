43fa56bf051e91fe8de969c04c9972ad0deca6ee
Leonardo Uribe <lu4242@gmail.com>
2012-04-23 21:16:01 -0500

    original code taken from http://code.google.com/p/perfbench/source/browse/trunk/perfbench thanks to Peter Thomas. jmeter-utils licensed under Apache License 2.0. Test applications were taken from seam 2.2.1 distribution, licensed under LGPL.

---
+++ perfbench-code/seam-jpa/src/main/java/seamjpa/seam/AuthenticatorAction.java
@@ -0,0 +1,33 @@
+package seamjpa.seam;
+
+import static org.jboss.seam.ScopeType.SESSION;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Out;
+import seamjpa.entity.User;
+
+@Name("authenticator")
+public class AuthenticatorAction {
+
+    @In
+    EntityManager em;
+    
+    @Out(required = false, scope = SESSION)
+    private User user;
+
+    public boolean authenticate() {
+        List results = em.createQuery("select u from User u where u.username = #{identity.username}"
+                + " and u.password = #{identity.password}").getResultList();
+        if (results.size() == 0) {
+            return false;
+        } else {
+            user = (User) results.get(0);
+            return true;
+        }
+    }
+}
